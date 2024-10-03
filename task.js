9281363fcfbcf300dc8da9a074651f88d75dacc9
Jerry0019 <157806653+Jerry0019@users.noreply.github.com>
2024-10-03 23:30:48 +0100

    Update task.js

--- task.js
+++ task.js
@@ -1,13 +1,14 @@
 // task.js
 
+// Class to represent a single task
 export class Task {
-    constructor(description) {
-      this.description = description;
-      this.isCompleted = false;
-    }
-  
-    toggleCompletion() {
-      this.isCompleted = !this.isCompleted;
-    }
+  constructor(description) {
+    this.description = description; // Description of the task
+    this.isCompleted = false; // Task starts as not completed
   }
-  
+
+  // Method to toggle the completion status of the task
+  toggleCompletion() {
+    this.isCompleted = !this.isCompleted;
+  }
+}
